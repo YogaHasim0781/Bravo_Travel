0beb7b4c4d52821fd9016b0071ef9af3d634db64
YogaHasim0781 <yoga65034@gmail.com>
2024-11-03 09:23:33 +0700

    ini update app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './Component/Navbar';
 import Home from './Component/Home';
+import Signin from './Component/Signin';
+import Signup from './Component/Signup';
+import BuyTicket from './Component/buyticket';
 
-const TravelHomepage = () => {
+const App = () => {
   return (
-    <div>
-      <Home />
-    </div>
-  );  
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/Buyticket" element={<BuyTicket />} />
+      </Routes>
+    </Router>
+  );
 };
 
-export default TravelHomepage;
+export default App;
