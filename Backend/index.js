c6dbddfe88b74179dd30c81df9d0cd468d82d8fe
Divoni Wesly <92214026+Divoniwesly@users.noreply.github.com>
2025-01-20 00:16:33 +0700

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,33 +1,44 @@
-import express from "express"
-import dotenv from "dotenv"
-import db from "./config/Database.js"
-import router from "./routes/index.js"
-import cors from "cors"
-import cookieParser from "cookie-parser"
+import express from "express";
+import dotenv from "dotenv";
+import db from "./config/Database.js";
+import router from "./routes/index.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import session from "express-session"; // Don't forget to import express-session
 
 dotenv.config();
-const app =  express();
+const app = express();
 
-try {
-    await db.authenticate();
-    console.log('Database connected')
-}   catch(error) { 
-    console.error(error)
+const startServer = async () => {
+    try {
+        await db.authenticate();
+        console.log('Database connected');
+    } catch (error) {
+        console.error(error);
+    }
+
+    app.use(session({
+        secret: process.env.SESS_SECRET,
+        resave: false,
+        saveUninitialized: true,
+        store: store,
+        cookie: {
+            httpOnly: true,
+            sameSite: 'lax',
+            secure: 'false',
+        }
+    }));
+
+    app.use(cors({
+        credentials: true,
+        origin: true,
+    }));
+
+    app.use(cookieParser());
+    app.use(express.json());
+    app.use(router);
+
+    app.listen(5000, () => console.log('Server is running at port 5000'));
 };
 
-app.use(cors({
-    credentials: true,
-    origin: (origin, callback) => {
-        if (!origin || /^(http:\/\/127\.0\.0\.1:\d{5})$/.test(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    }
-}));
-
-app.use(cookieParser())
-app.use(express.json())
-app.use(router)
-
-app.listen(5000, () => console.log('Server is running at port 5000'))
+startServer();
